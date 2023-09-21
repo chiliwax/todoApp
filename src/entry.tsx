d22d0738e2abbc3264bcd3ef1d9232c93a5d988e
Thibault Lecointe <thibault.lecointe@gimbalcube.com>
2023-09-21 21:00:42 +0200

    test

--- src/entry.tsx
+++ src/entry.tsx
@@ -5,3 +5,29 @@ import {App as ExpoRootComponent} from 'expo-router/_app'
 const rootComponent = process.env.EXPO_PUBLIC_STORYBOOK_ENABLE === 'TRUE' ? StoryBoardRootComponent : ExpoRootComponent
 
 registerRootComponent(rootComponent);
+
+const routes = {
+    home: '/',
+    profile: '/profile'
+} as const
+
+type KeyOfRoutes = keyof typeof routes
+type Routes = typeof routes [KeyOfRoutes]
+
+const goToRoute = (route: Routes) => {
+
+}
+
+
+const LOG_LEVEL = {
+    DEBUG: 'DEBUG',
+    WARNING: 'WARNING'
+} as const
+type ObjectValue<T> = T[keyof T];
+type LogLevel = ObjectValue<typeof LOG_LEVEL>
+
+const log = (msg: string, logLevel: LogLevel) => {
+
+}
+
+log('hello', LOG_LEVEL.DEBUG)

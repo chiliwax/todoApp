68b897feedf9e81dc67173a2be937a778a203662
Thibault Lecointe <thibault.lecointe@gimbalcube.com>
2023-09-21 21:06:05 +0200

    test

--- src/entry.tsx
+++ src/entry.tsx
@@ -2,6 +2,7 @@ import { registerRootComponent } from "expo";
 import StoryBoardRootComponent from './.storybook';
 import {App as ExpoRootComponent} from 'expo-router/_app'
 
+
 const rootComponent = process.env.EXPO_PUBLIC_STORYBOOK_ENABLE === 'TRUE' ? StoryBoardRootComponent : ExpoRootComponent
 
 registerRootComponent(rootComponent);
